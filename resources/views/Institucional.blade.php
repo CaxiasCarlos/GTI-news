72a69100fc6f766b93559bf4117e10a3eeb0003c
CaxiasCarlos <carloscaxias999@gmail.com>
2024-11-28 10:23:28 -0300

    commit final, "tudo funcionando"

---
+++ resources/views/Institucional.blade.php
@@ -0,0 +1,10 @@
+<x-base>
+
+<div class="full-screen">
+<img src="{{asset('imagens/2brother.png')}}"/>
+<img src="{{asset('imagens/brother.png')}}"/>
+<img src="{{asset('imagens/1brother.png')}}" class="-my-1 -ml-0.5"  />
+</div>
+
+
+</x-base>
---
+++ resources/views/sobre-o-curso.blade.php
@@ -0,0 +1,21 @@
+<x-base>
+
+
+<div class="p-10 mx-auto justify-center flex " >
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/KNYM-qLGsHo?si=BzNqiGSnHZRgzqmc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+</div>
+
+
+
+<div class="text-center  justify-center  break-word px-36  "  >
+
+O Centro Paula Souza mantém 79 Faculdades de Tecnologia (Fatecs) distribuídas em 72 municípios paulistas. As Fatecs atendem mais de 90 mil alunos, matriculados nos 96 cursos de graduação tecnológica presenciais e um na modalidade à distância, que têm uma carga horária de 2.400 horas, com duração mínima de três anos.
+
+</div>
+
+
+
+
+</x-base>
